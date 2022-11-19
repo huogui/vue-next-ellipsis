c18f70748da376fc91be74a724ad98e44cdadfba
huogui <645039234@qq.com>
2022-11-19 13:58:01 +0800

    chore: test preview

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pkg from './package.json'
 
+
+console.log(pkg.name)
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${pkg.name}/`,
+  base: './',
   plugins: [vue()],
 })
